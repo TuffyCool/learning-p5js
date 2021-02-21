0b0612f6b0cc9aea382d4ab344d1c8de085e818f
TuffyCool <james.baileysmith17@gmail.com>
2021-02-21 18:54:15 -0500

    added random walker code from vectors - OOP

--- NatureofCode_Intro/3-Vector math/sketch.js
+++ NatureofCode_Intro/3-Vector math/sketch.js
@@ -1,6 +1,11 @@
 function setup() {
 	createCanvas(400, 400);
+	background(0);
+	walker = new walker(200, 200);
+
 }
 
 function draw() {
-} 
+	walker.update();
+	walker.show();
+}
---
+++ NatureofCode_Intro/3-Vector math/walker.js
@@ -0,0 +1,14 @@
+class walker {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+  }
+  update() {
+    this.pos.x = this.pos.x + random(-1, 1);
+    this.pos.y = this.pos.y + random(-1, 1);
+  }
+  show() {
+    stroke(255, 100);
+    strokeWeight(2);
+    point(this.pos.x, this.pos.y);
+  }
+}
