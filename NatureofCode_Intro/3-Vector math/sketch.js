a899c7c9454fa172e049602ace8d5506f5d0995d
TuffyCool <james.baileysmith17@gmail.com>
2021-02-21 19:07:53 -0500

    Added vector math code

--- NatureofCode_Intro/3-Vector math/sketch.js
+++ NatureofCode_Intro/3-Vector math/sketch.js
@@ -1,11 +1,10 @@
 function setup() {
 	createCanvas(400, 400);
-	background(0);
 	walker = new walker(200, 200);
-
 }
 
 function draw() {
+	background(0);
 	walker.update();
 	walker.show();
 }
--- NatureofCode_Intro/3-Vector math/walker.js
+++ NatureofCode_Intro/3-Vector math/walker.js
@@ -1,14 +1,22 @@
 class walker {
   constructor(x, y) {
     this.pos = createVector(x, y);
+    this.vel = createVector(1, -1);
   }
   update() {
-    this.pos.x = this.pos.x + random(-1, 1);
-    this.pos.y = this.pos.y + random(-1, 1);
+
+    // Adds the vector to the dot's position each frame
+    // with the vector being (1,-1)
+    this.pos.add(this.vel)
+    // this.pos.x = this.pos.x + this.vel.x;
+    // this.pos.y = this.pos.y + this.vel.y;
+
+    // this.pos.x = this.pos.x + random(-1, 1);
+    // this.pos.y = this.pos.y + random(-1, 1);
   }
   show() {
     stroke(255, 100);
-    strokeWeight(2);
+    strokeWeight(20);
     point(this.pos.x, this.pos.y);
   }
 }
